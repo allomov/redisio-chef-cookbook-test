45f3aed7b7662745862a39277228d09325e02d51
A.S. Lomoff <lomov.as@gmail.com>
2013-11-28 15:30:32 +0300

    fix master installation script

--- roles/redis_master.rb
+++ roles/redis_master.rb
@@ -1,33 +1,20 @@
 name 'redis-master'
 description 'Redis Master Node'
 
-server_name = 'master'
+run_list 'recipe[redisio]', 'recipe[redisio::install]', 'recipe[redisio::enable]'
 
-run_list 'recipe[redisio]', 'recipe[redisio::enable]'
-
+redis_master_config = Chef::DataBagItem.load('redis', 'master')
 
 default_attributes({
   'redisio' => {
-    'default_settings' => {'datadir' => '/mnt/redis'},
-    'servers' => [{'port' => '6379'}, {'port' => '6380', 'name' => "MyInstance"}]
+    'servers' => [
+      {
+        'job_control' => 'initd',
+        'name' => 'master',
+        'backuptype' => 'both',
+        'port' => redis_master_config['port']
+      }
+    ]
   }
 })
 
-# # redis_master_config = Chef::DataBagItem.load('redis', 'master')
-
-# default_attributes({})
-
-# default_attributes({
-#   'redisio' => {
-#     'servers' => [
-#       {
-#         'job_control' => 'initd',
-#         'name' => server_name,
-#         'backuptype' => 'both',
-#         'port' => redis_master_config['port']
-#       }
-#     ]
-#   }
-# })
-
-Chef::Log.info("Compiling Redis Master setup")
