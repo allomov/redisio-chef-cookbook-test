5896603fc885b783a8a915bfdd6c4e0efd93f3f5
A.S. Lomoff <lomov.as@gmail.com>
2013-12-04 00:24:34 +0300

    add requested changes to config: mode and save options added

--- roles/redis_master.rb
+++ roles/redis_master.rb
@@ -10,13 +10,21 @@ run_list 'recipe[redisio]',
 
 redis_master_config = Chef::DataBagItem.load('redis', 'master')
 
+mode = redis_master_config['mode'] 
+backuptype_converter = {'cache' => 'rdb', 'transient' => 'rdb', 'acid' => 'both'}
+backuptype = backuptype_converter[mode]
+
+save_config = (mode == 'cache') ? [] : (redis_master_config['save'] || ["900 1", "300 10", "60 10000"])
+
 default_attributes({
   'redisio' => {
     'servers' => [
       {
         'name' => 'master',
         'backuptype' => 'both',
-        'port' => redis_master_config['port']
+        'port' => redis_master_config['port'], 
+        'backuptype' => backuptype,
+        'save' => save_config
       }
     ]
   }
